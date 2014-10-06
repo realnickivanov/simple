a7114c2484ba7f2829610abbdc206d2b4905dde7
ivanmuzika <ivan.muzika@gmail.com>
2014-10-06 11:34:18 +0300

    Update main-build.js. Revert specs.html and settings.js.

--- settings.js
+++ settings.js
@@ -1,1 +1,24 @@
-﻿{"logo":{"url":""},"xApi":{"enabled":true,"lrs":{"uri":"http://cloud.scorm.com/ScormEngineInterface/TCAPI/public","authenticationRequired":false,"credentials":{}},"allowedVerbs":["started","stopped","experienced","mastered","answered","passed","failed"]},"masteryScore":{"score":100},"theme":{"key":"cartoon"}}
+﻿{
+    "logo": {
+        "url": ""
+    },
+    "theme": {
+        "key": ""
+    },
+    "xApi": {
+        "enabled": false,
+        "lrs": {
+            "uri": "http://cloud.scorm.com/ScormEngineInterface/TCAPI/public/statements",
+            "credentials": {
+                "username": "",
+                "password": ""
+            },
+          "authenticationRequired": false
+        },
+        "allowedVerbs":["started","stopped","experienced","mastered","answered","passed","failed"]
+    },
+    "masteryScore": {
+        "score": "75"
+    }
+}
+
