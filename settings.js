53257c06d2dac6b0c797287a561deb968e2566cb
vasyl-ivanchuk <vasyl.ivanchuk@gmail.com>
2014-11-03 12:10:53 +0200

    1. Change translation key names. 2. Change order of translation keys. 3. Add missed translations.

--- settings.js
+++ settings.js
@@ -1,145 +1,66 @@
-{  
-    "logo":{  
-        "url":""
+{
+    "logo": {
+        "url": ""
     },
-   "xApi":{  
-       "enabled":true,
-       "lrs":{  
-           "uri":"",
-           "authenticationRequired":false,
-           "credentials":{  
-               "username":"",
-               "password":""
-           }
-       },
-      "allowedVerbs":[  
-         "started",
-         "stopped",
-         "experienced",
-         "mastered",
-         "answered",
-         "passed",
-         "failed"
-      ]
-   },
-   "masteryScore":{  
-       "score":100
-   },
-   "theme":{  
-       "key":"flat"
-   },
-   "translations":[  
-      {  
-          "key":"[course]",
-          "value":"????:"
-      },
-      {  
-          "key":"[start course]",
-          "value":"?????? ????"
-      },
-      {  
-          "key":"[finish course]",
-          "value":"????????? ????"
-      },
-      {  
-          "key":"[xapi header]",
-          "value":"???? ???? ??? xapi"
-      },
-      {  
-          "key":"[xapi description]",
-          "value":"???? ?????, ??????? ???? ???? ? ????????? 'Start and report my progress' to enable progress tracking. Otherwise, click 'Do not report, just start'."
-      },
-      {  
-          "key":"[xapi name placeholder]",
-          "value":"???? ???"
-      },
-      {  
-          "key":"[xapi email placeholder]",
-          "value":"??? ?????"
-      },
-      {  
-          "key":"[xapi name is not valid]",
-          "value":"?? ???? ??????????"
-      },
-      {  
-          "key":"[xapi email is not valid]",
-          "value":"??????? ???????? ?????"
-      },
-      {  
-          "key":"[xapi do not report]",
-          "value":"?? ?????????, ?????? ???"
-      },
-      {  
-          "key":"[xapi report and start course]",
-          "value":"?????? ???? ? ????????? ???????"
-      },
-      {  
-          "key":"[learning objectives]",
-          "value":"???? ????????:"
-      },
-      {  
-          "key":"[start]",
-          "value":"?????"
-      },
-      {  
-          "key":"[home]",
-          "value":"????"
-      },
-      {  
-          "key":"[learning content]",
-          "value":"?????????? ????????"
-      },
-      {  
-          "key":"[submit]",
-          "value":"???????????"
-      },
-      {  
-          "key":"[try again]",
-          "value":"?????????? ??"
-      },
-      {  
-          "key":"[next]",
-          "value":"????"
-      },
-      {  
-          "key":"[correct answer]",
-          "value":"????????? ?????????"
-      },
-      {  
-          "key":"[incorrect answer]",
-          "value":"??????? ?????????"
-      },
-      {  
-          "key":"[not found message]",
-          "value":"???????, ??? ?????? ???? ?? ??????? ???????????. ??????? ???? ? ??? =)"
-      },
-      {  
-          "key":"[something is wrong]",
-          "value":"???????, ???? ?? ???"
-      },
-      {  
-          "key":"[skip reporting]",
-          "value":"???? ?? ??????????, ??? ??????? ?? ???? ?????????."
-      },
-      {  
-          "key":"[restart course]",
-          "value":"?????? ?????"
-      },
-      {  
-          "key":"[continue anyway]",
-          "value":"??? ???? ??????????"
-      },
-      {  
-          "key":"[previous question]",
-          "value":"?????"
-      },
-      {  
-          "key":"[next question]",
-          "value":"??????"
-      },
-      {
-          "key":"[thank you message]",
-          "value":"????? ?? ??????????? ?????! ??????? ????? ???????!"
-      }
-   ]
+    "theme": {
+        "key": ""
+    },
+    "xApi": {
+        "enabled": false,
+        "lrs": {
+            "uri": "http://cloud.scorm.com/ScormEngineInterface/TCAPI/public/statements",
+            "credentials": {
+                "username": "",
+                "password": ""
+            },
+          "authenticationRequired": false
+        },
+        "allowedVerbs":["started","stopped","experienced","mastered","answered","passed","failed"]
+    },
+    "masteryScore": {
+        "score": "75"
+    },
+    "translations": [
+            { "key": "[course]", "value": "Course" },
+            { "key": "[start course]", "value": "Start course" },
+            { "key": "[finish course]", "value": "Finish course" },
+            { "key": "[learning objectives]", "value": "Learning objectives:" },
+            { "key": "[start]", "value": "Start" },
+            { "key": "[home]", "value": "Home" },
+            { "key": "[learning content]", "value": "Learning content" },
+            { "key": "[submit]", "value": "Submit" },
+            { "key": "[try again]", "value": "Try again" },
+            { "key": "[next]", "value": "Next" },
+            { "key": "[correct answer]", "value": "Correct answer" },
+            { "key": "[incorrect answer]", "value": "Incorrect answer" },
+            { "key": "[previous question]", "value": "previous" },
+            { "key": "[next question]", "value": "next" },
+            { "key": "[text matching question hint]", "value": "Drag items from right column to the left to match the pairs" },
+            { "key": "[text matching question drop here]", "value": "Drop here" },
+            { "key": "[statement question true text]", "value": "True" },
+            { "key": "[statement question false text]", "value": "False" },
+            { "key": "[drag and drop question all texts are placed]", "value": "All texts are placed" },
+            { "key": "[drag and drop question drop here]", "value": "Drop here" },
+            { "key": "[thank you message]", "value": "Thank you. It is now safe to close this page." },
+            { "key": "[there are no questions]", "value": "No questions" },
+            { "key": "[browser not supported]", "value": "Your browser is currently not supported." },
+            { "key": "[browser not supported hint]", "value": "Don’t worry, there is an easy fix. All you have to do is click one of the icons below and follow the instructions." },
+            { "key": "[page not found title]", "value": "Page not found (404)" },
+            { "key": "[page not found message]", "value": "Sorry, the page you have been looking for has not been found. Try checking the URL on errors, use the navigation above or click 'Home' link below." },
+            { "key": "[tracking and tracing header]", "value": "Your credentials for progress tracking" },
+            { "key": "[tracking and tracing hint]", "value": 'Please enter your credentials and click "Start and report my progress" to enable progress tracking. Otherwise, click "Do not report, just start".' },
+            { "key": "[tracking and tracing name field]", "value": "Your name" },
+            { "key": "[tracking and tracing email field]", "value": "Your e-mail" },
+            { "key": "[tracking and tracing name is not valid]", "value": "Fill in your name" },
+            { "key": "[tracking and tracing email is not valid]", "value": "Enter a valid e-mail" },
+            { "key": "[tracking and tracing skip reporting]", "value": "Do not report, just start" },
+            { "key": "[tracking and tracing start]", "value": "Start and report my progress" },
+            { "key": "[tracking and tracing error]", "value": "Something is wrong" },
+            { "key": "[tracking and tracing error hint]", "value": "If you continue without restarting, your learning progress will not be reported." },
+            { "key": "[tracking and tracing restart course]", "value": "Restart course" },
+            { "key": "[tracking and tracing continue anyway]", "value": "Continue anyway" },
+            { "key": "[tracking and tracing reporting progress]", "value": "reporting progress..." },
+            { "key": "[tracking and tracing not supported]", "value": "Progress tracking cannot be established" },
+            { "key": "[tracking and tracing not supported hint]", "value": "Sorry, this course does not support progress tracking in Internet Explorer 9. Please use one of the following browser: Chrome, Firefox, Safari, IE10+ in order to track your progress, or just start the course without tracking." }
+        ]
 }
