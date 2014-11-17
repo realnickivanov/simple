d479c916e66bc1db64c4f129b5e4344785ccca32
Romsters <petriv.roma@gmail.com>
2014-11-17 12:25:06 +0200

    #30: Adjust tables in CKEditor

--- app/bootstrapping/knockoutBindings/elementsWrapBinding.js
+++ app/bootstrapping/knockoutBindings/elementsWrapBinding.js
@@ -17,7 +17,6 @@
                 var $table = $(table),
                     $wrapper = $(tableWrapper);
 
-                //$table.css('width', '100%').css('height', 'auto').attr('align', null);
                 $table.wrap($wrapper);
             });
         }
