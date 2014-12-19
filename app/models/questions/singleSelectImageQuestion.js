7021f205909c26634b3e314438b3c0aaa4dee618
Andrey Drebot <a.drebot@gmail.com>
2014-12-19 13:55:49 +0200

    Save and restore progress for single select image

--- app/models/questions/singleSelectImageQuestion.js
+++ app/models/questions/singleSelectImageQuestion.js
@@ -3,12 +3,17 @@
         "use strict";
 
         function SingleSelectImageQuestion(spec) {
-            Question.call(this, spec);
+            var _protected = {
+                getProgress: getProgress,
+                restoreProgress: restoreProgress,
+
+                submit: submitAnswer
+            };
+
+            Question.call(this, spec, _protected);
+
             this.correctAnswerId = spec.correctAnswerId;
             this.checkedAnswerId = null;
-
-            this.submitAnswer = submitAnswer;
-
             this.answers = _.map(spec.answers, function (answer) {
                 return new CheckableImageAnswer({
                     id: answer.id,
@@ -34,6 +39,19 @@
 
         function calculateScore(answerId, correctAnswerId) {
             return answerId != correctAnswerId ? 0 : 100;
+        }
+
+        function getProgress() {
+            if (this.isCorrectAnswered) {
+                return 100;
+            } else {
+                return this.checkedAnswerId;
+            }
+        }
+
+        function restoreProgress(progress) {
+            this.checkedAnswerId = progress === 100 ? this.correctAnswerId : progress;
+            this.score(calculateScore(this.checkedAnswerId, this.correctAnswerId));
         }
 
     });
