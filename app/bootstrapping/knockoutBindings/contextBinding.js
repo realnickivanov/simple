21b59cba510c1cc2a0c5db70ee257b5ca1e0dbc2
Andrey Drebot <a.drebot@gmail.com>
2015-01-09 10:16:24 +0200

    Load publish settings before template settings

---
+++ app/bootstrapping/knockoutBindings/contextBinding.js
@@ -0,0 +1,12 @@
+﻿define([], function () {
+
+    ko.bindingHandlers.context = {
+        init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+            viewModel.__context__ = element.getContext('2d');
+        },
+        update: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+            var callback = ko.utils.unwrapObservable(allBindingsAccessor().contextCallback);
+            callback.call(viewModel, viewModel.__context__);
+        }
+    };
+});
