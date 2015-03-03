5b872ea7277e95b3c519d9200b28e7da4fc53907
dmitry-zaets <dmitry.zaets@gmail.com>
2015-03-03 17:48:49 +0200

    Added mock for localization

--- settings/js/localize.js
+++ settings/js/localize.js
@@ -8,7 +8,20 @@
         'cartoon': 'Cartoon light',
         'grey': 'Grey scheme',
         'black': 'Black scheme',
-        'flat': 'Flat scheme'
+        'flat': 'Flat scheme',
+        'default': 'easygenerator (recommended)',
+        'custom': 'Custom LRS',
+
+        'xx': 'Custom',
+        'cn': 'Chinese',
+        'de': 'German',
+        'en': 'English',
+        'fr': 'French',
+        'it': 'Italian',
+        'nl': 'Dutch',
+        'tr': 'Turkish',
+        'ua': 'Ukrainian'
+
     };
 
-})(window.app || {});
+})(window.app = window.app || {});
