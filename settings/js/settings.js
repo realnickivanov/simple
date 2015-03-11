df0ddeae3ef6f6879805ff891aacb5fbb4b30a6d
dmitry-zaets <dmitry.zaets@gmail.com>
2015-03-11 17:58:41 +0200

    Fixed image uploader (Dependency on viewmodel still exists)

--- settings/js/settings.js
+++ settings/js/settings.js
@@ -5,6 +5,7 @@
         var bName = b.name.toLowerCase();
         return ((aName < bName) ? -1 : ((aName > bName) ? 1 : 0));
     }
+
     var
         currentSettings = null,
         currentExtraData = null,
@@ -17,7 +18,7 @@
         themes: new app.ThemesModel(),
 
         masteryScore: ko.observable(100),
-       
+
         userHasStarterPlan: ko.observable(false)
     };
 
@@ -48,7 +49,7 @@
         var settings = viewModel.getSettingsData(),
             extraData = viewModel.getExtraData(),
             newSettings = JSON.stringify(settings),
-            newExtraData =JSON.stringify(extraData);
+            newExtraData = JSON.stringify(extraData);
 
         if (JSON.stringify(currentSettings) === newSettings && JSON.stringify(currentExtraData) === newExtraData) {
             return;
@@ -72,7 +73,6 @@
 
             if (user.accessType > 0) {
                 viewModel.userHasStarterPlan(true);
-                imageUploader.init();
             }
 
             if (settings.xApi) {
@@ -131,122 +131,127 @@
             //TODO: should show error
         });
     }
-    
+
     //#region Image uploader
 
-    var imageUploader = {
-        apiUrl: baseURL + '/storage/image/upload',
-        maxFileSize: 10, //MB
-        supportedExtensions: ['jpeg', 'jpg', 'png', 'bmp', 'gif'],
-        somethingWentWrongMessage: { title: 'Something went wrong', description: 'Please, try again' },
+    function imageUploaderViewModel() {
+        var imageUploader = {
+            apiUrl: baseURL + '/storage/image/upload',
+            maxFileSize: 10, //MB
+            supportedExtensions: ['jpeg', 'jpg', 'png', 'bmp', 'gif'],
+            somethingWentWrongMessage: { title: 'Something went wrong', description: 'Please, try again' },
 
-        status: {
-            default: function () {
-                viewModel.logo.setDefaultStatus();
+            status: {
+                default: function () {
+                    viewModel.logo.setDefaultStatus();
+                },
+                fail: function (reason) {
+                    viewModel.logo.setFailedStatus(reason.title, reason.description);
+                },
+                loading: function () {
+                    viewModel.logo.isLoading(true);
+                }
             },
-            fail: function (reason) {
-                viewModel.logo.setFailedStatus(reason.title, reason.description);
-            },
-            loading: function () {
-                viewModel.logo.isLoading(true);
-            }
-        },
 
-        button: {
-            enable: function () {
-                this.$input.attr('disabled', false).closest('.image-upload-button').removeClass('disabled');
+            button: {
+                enable: function () {
+                    this.$input.attr('disabled', false).closest('.image-upload-button').removeClass('disabled');
+                },
+                disable: function () {
+                    this.$input.attr('disabled', true).closest('.image-upload-button').addClass('disabled');
+                },
+                submit: function () {
+                    this.$input[0].form.submit();
+                    this.disable();
+                    imageUploader.status.loading();
+                },
+                $input: $('#logoInput')
             },
-            disable: function () {
-                this.$input.attr('disabled', true).closest('.image-upload-button').addClass('disabled');
+
+            init: function () {
+                imageUploader.button.$input.on('change', imageUploader.processFile);
+                imageUploader.button.enable();
             },
-            submit: function () {
-                this.$input[0].form.submit();
-                this.disable();
+
+            processFile: function () {
+                if (!this.files) {
+                    imageUploader.button.submit();
+                    return;
+                }
+                if (this.files.length === 0) {
+                    return;
+                }
+
+                var file = this.files[0],
+                    fileExtension = file.name.split('.').pop().toLowerCase();
+
+                if ($.inArray(fileExtension, imageUploader.supportedExtensions) === -1) {
+                    imageUploader.status.fail({ title: 'Unsupported image format', description: '(Allowed formats: ' + imageUploader.supportedExtensions.join(', ') + ')' });
+                    return;
+                }
+                if (file.size > imageUploader.maxFileSize * 1024 * 1024) {
+                    imageUploader.status.fail({ title: 'File is too large', description: '(Max file size: ' + imageUploader.maxFileSize + 'MB)' });
+                    return;
+                }
+                imageUploader.uploadFile(file);
+            },
+
+            uploadFile: function (file) {
+                imageUploader.button.disable();
                 imageUploader.status.loading();
+
+                var formData = new FormData();
+                formData.append('file', file);
+
+                $.ajax({
+                    url: imageUploader.apiUrl,
+                    type: 'POST',
+                    data: formData,
+                    cache: false,
+                    dataType: 'json',
+                    processData: false, // Don't process the files
+                    contentType: false, // Set content type to false as jQuery will tell the server its a query string request
+                }).done(function (response) {
+                    imageUploader.handleResponse(response);
+                }).fail(function () {
+                    imageUploader.status.fail(imageUploader.somethingWentWrongMessage);
+                    imageUploader.button.enable();
+                });
             },
-            $input: $('#logoInput')
-        },
 
-        init: function () {
-            imageUploader.button.$input.on('change', imageUploader.processFile);
-            imageUploader.button.enable();
-        },
+            handleResponse: function (response) {
+                try {
+                    if (!response) {
+                        throw 'Response is empty';
+                    }
 
-        processFile: function () {
-            if (!this.files) {
-                imageUploader.button.submit();
-                return;
-            }
-            if (this.files.length === 0) {
-                return;
-            }
+                    if (typeof response != 'object') {
+                        response = JSON.parse(response);
+                    }
 
-            var file = this.files[0],
-                fileExtension = file.name.split('.').pop().toLowerCase();
+                    if (!response || !response.success || !response.data) {
+                        throw 'Request is not success';
+                    }
 
-            if ($.inArray(fileExtension, imageUploader.supportedExtensions) === -1) {
-                imageUploader.status.fail({ title: 'Unsupported image format', description: '(Allowed formats: ' + imageUploader.supportedExtensions.join(', ') + ')' });
-                return;
-            }
-            if (file.size > imageUploader.maxFileSize * 1024 * 1024) {
-                imageUploader.status.fail({ title: 'File is too large', description: '(Max file size: ' + imageUploader.maxFileSize + 'MB)' });
-                return;
-            }
-            imageUploader.uploadFile(file);
-        },
-
-        uploadFile: function (file) {
-            imageUploader.button.disable();
-            imageUploader.status.loading();
-
-            var formData = new FormData();
-            formData.append('file', file);
-
-            $.ajax({
-                url: imageUploader.apiUrl,
-                type: 'POST',
-                data: formData,
-                cache: false,
-                dataType: 'json',
-                processData: false, // Don't process the files
-                contentType: false, // Set content type to false as jQuery will tell the server its a query string request
-            }).done(function (response) {
-                imageUploader.handleResponse(response);
-            }).fail(function () {
-                imageUploader.status.fail(imageUploader.somethingWentWrongMessage);
-                imageUploader.button.enable();
-            });
-        },
-
-        handleResponse: function (response) {
-            try {
-                if (!response) {
-                    throw 'Response is empty';
+                    viewModel.logo.setUrl(response.data.url);
+                    imageUploader.status.default();
+                } catch (e) {
+                    imageUploader.status.fail(imageUploader.somethingWentWrongMessage);
+                } finally {
+                    imageUploader.button.enable();
                 }
-
-                if (typeof response != 'object') {
-                    response = JSON.parse(response);
-                }
-
-                if (!response || !response.success || !response.data) {
-                    throw 'Request is not success';
-                }
-
-                viewModel.logo.setUrl(response.data.url);
-                imageUploader.status.default();
-            } catch (e) {
-                imageUploader.status.fail(imageUploader.somethingWentWrongMessage);
-            } finally {
-                imageUploader.button.enable();
             }
         }
-    };
+        return imageUploader;
+    }
 
     //#endregion Image uploader
 
     viewModel.init().done(function () {
         $(document).ready(function () {
             ko.applyBindings(viewModel, $('.settings-container')[0]);
+            var imageUploader = new imageUploaderViewModel();
+            imageUploader.init();
         });
     });
 
