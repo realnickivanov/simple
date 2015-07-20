f40eb09640a3e43d88e3131039b49a29cbd2b256
LeonidPelyhivskyi <plo.91ua@gmail.com>
2015-07-20 12:00:55 +0300

    Add course title binding. Progress indicator on objective is too thick

---
+++ app/bootstrapping/knockoutBindings/courseTitleBindingHandler.js
@@ -0,0 +1,15 @@
+﻿define(['knockout', 'jquery', 'durandal/composition'], function (ko, $, composition) {
+    'use strict';
+    ko.bindingHandlers.courseTitle = {
+        init: function (element) {
+            var $element = $(element),
+                lenght = element.innerHTML.length;
+
+            if (lenght > 100) {
+                $element.css('font-size', '22px');
+            }
+        }
+    };
+
+    composition.addBindingHandler('courseTitle');
+})
