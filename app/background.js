d104ada797a4987f1cc454572266ca4819c90eee
LeonidPelyhivskyi <plo.91ua@gmail.com>
2015-07-23 13:02:30 +0300

    Change background shadow

--- app/background.js
+++ app/background.js
@@ -8,42 +8,41 @@ define([], function () {
 
         var element = $('.header');
 
-        if (!(_.isNullOrUndefined(background) && _.isNullOrUndefined(background.image) && _.isNullOrUndefined(background.image.src))) {
-            var image = new Image(),
+        if (!background || !background.image || !background.image.src) {
+            element.addClass('default-background');
+            return;
+        }
+ 
+        var image = new Image(),
             src = background.image.src,
             position = '0 0',
             repeat = 'no-repeat',
             size = 'auto';
 
 
-            if (background.image.type === 'repeat') {
-                repeat = 'repeat';
-            }
-
-            if (background.image.type === 'fullscreen') {
-                size = 'cover';
-                position = 'center';
-            }
-
-            image.onload = function () {
-                $(element)
-                    .css({
-                        'top': '0',
-                        'bottom': '0',
-                        'background-image': 'url(' + src + ')',
-                        'background-position': position,
-                        '-webkit-background-size': size,
-                        'background-size': size,
-                        'background-repeat': repeat
-                    });
-            };
-
-            image.src = src;
-            return;
+        if (background.image.type === 'repeat') {
+            repeat = 'repeat';
         }
- 
 
-        element.addClass('default-background');
+        if (background.image.type === 'fullscreen') {
+            size = 'cover';
+            position = 'center';
+        }
+
+        image.onload = function () {
+            $(element)
+                .css({
+                    'top': '0',
+                    'bottom': '0',
+                    'background-image': 'url(' + src + ')',
+                    'background-position': position,
+                    '-webkit-background-size': size,
+                    'background-size': size,
+                    'background-repeat': repeat
+                });
+        }
+
+        image.src = src;
     }
 
 });
