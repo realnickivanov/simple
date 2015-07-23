7ca62e76f929144dd4ecb8fd46cdbc2e50f51cb8
LeonidPelyhivskyi <plo.91ua@gmail.com>
2015-07-23 12:05:12 +0300

    Fix bug with background. Fix finish course btn.

--- app/background.js
+++ app/background.js
@@ -2,47 +2,48 @@ define([], function () {
 
     return {
         apply: apply
-    }
+    };
 
     function apply(background) {
 
         var element = $('.header');
 
-        if (_.isNullOrUndefined(background) && _.isNullOrUndefined(background.image) && _.isNullOrUndefined(background.image.src)) {
-            element.addClass('default-background');
-            return;
-        }
- 
-        var image = new Image(),
+        if (!(_.isNullOrUndefined(background) && _.isNullOrUndefined(background.image) && _.isNullOrUndefined(background.image.src))) {
+            var image = new Image(),
             src = background.image.src,
             position = '0 0',
             repeat = 'no-repeat',
             size = 'auto';
 
 
-        if (background.image.type === 'repeat') {
-            repeat = 'repeat';
-        }
+            if (background.image.type === 'repeat') {
+                repeat = 'repeat';
+            }
 
-        if (background.image.type === 'fullscreen') {
-            size = 'cover';
-            position = 'center';
-        }
+            if (background.image.type === 'fullscreen') {
+                size = 'cover';
+                position = 'center';
+            }
 
-        image.onload = function () {
-            $(element)
-                .css({
-                    'top': '0',
-                    'bottom': '0',
-                    'background-image': 'url(' + src + ')',
-                    'background-position': position,
-                    '-webkit-background-size': size,
-                    'background-size': size,
-                    'background-repeat': repeat
-                });
-        }
+            image.onload = function () {
+                $(element)
+                    .css({
+                        'top': '0',
+                        'bottom': '0',
+                        'background-image': 'url(' + src + ')',
+                        'background-position': position,
+                        '-webkit-background-size': size,
+                        'background-size': size,
+                        'background-repeat': repeat
+                    });
+            };
 
-        image.src = src;
+            image.src = src;
+            return;
+        }
+ 
+
+        element.addClass('default-background');
     }
 
 });
