b51df766ce64257fd8979f62c42db6f6c1ae38d3
Romsters <petriv.roma@gmail.com>
2015-12-07 17:39:31 +0200

    fix hint in Scenario question

---
+++ app/bootstrapping/knockoutBindings/customFocusBinding.js
@@ -0,0 +1,33 @@
+﻿define(function () {
+    ko.bindingHandlers.customFocus = {
+        init: function (element, valueAccessor) {
+            var $element = $(element),
+                value = valueAccessor(),
+                focusClass = value.class || 'focused';
+
+            $('html').bind('touchstart', removeFocusClass);
+            $('html').bind('click', removeFocusClass);
+            $(window).bind('blur', removeFocusClass);
+
+            ko.utils.domNodeDisposal.addDisposeCallback(element, function () {
+                $('html').unbind('touchstart', removeFocusClass);
+                $('html').unbind('click', removeFocusClass);
+                $(window).unbind('blur', removeFocusClass);
+            });
+
+            $element.on('touchstart', function (e) {
+                e.stopPropagation();
+            });
+
+            $element.on('click', function (e) {
+                $element.addClass(focusClass);
+                e.stopPropagation();
+            });
+
+            function removeFocusClass() {
+                $element.removeClass(focusClass);
+            }
+
+        }
+    }
+});
