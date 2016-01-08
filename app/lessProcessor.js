158a955cd64066fb8b77bbbf1eff46ef7c3c4f1f
AndreyBaranov <vfreiby@gmail.com>
2016-01-08 15:10:44 +0200

    removed saving of processed less in localstorage

--- app/lessProcessor.js
+++ app/lessProcessor.js
@@ -8,11 +8,28 @@
 
     function init(colors) {
 
+        clearLocalStorage();
+
         _.each(colors, function (pair) {
             lessProcessor.vars[pair.key] = pair.value;
         })
 
         return less.modifyVars(lessProcessor.vars);
     };
+
+    function clearLocalStorage() {
+        if (!window.localStorage || !less) {
+            return;
+        }
+        var host = window.location.host;
+        var protocol = window.location.protocol;
+        var keyPrefix = protocol + '//' + host + '/css/colors.css';
+
+        for (var key in window.localStorage) {
+            if (key.indexOf(keyPrefix) === 0) {
+                delete window.localStorage[key];
+            }
+        }
+    }
 
 });
