ac5447a3e9c85b87f6c68a31b9ff1ec5033b109c
vasyl-ivanchuk <vasyl.ivanchuk@gmail.com>
2016-05-23 15:32:20 +0300

    Include images and fonts into the package

--- app/modules/webFontLoaderProvider.js
+++ app/modules/webFontLoaderProvider.js
@@ -1,4 +1,4 @@
-﻿define(['WebFont'], function (fontLoader) {
+﻿define(['WebFont', 'manifestReader'], function(fontLoader, manifestReader) {
 
 
     return {
@@ -8,30 +8,53 @@
     function init(fonts) {
         var defer = Q.defer();
 
-        var familiesToLoad = _.chain(fonts)
-            .map(function (font) {
-                return font.fontFamily
-            })
-            .uniq()
-            .without('Arial', 'Times new roman', 'Verdana') //filter font families that are available in all supported browsers
-            .value();
+        manifestReader.readManifest().then(function(manifest) {
+            var familiesToLoad = _.chain(fonts)
+                .map(function(font) {
+                    return font.fontFamily
+                })
+                .uniq()
+                .without('Arial', 'Times new roman', 'Verdana') //filter font families that are available in all supported browsers
+                .value();
 
-        fontLoader.load({
-            google: {
-                families: familiesToLoad && familiesToLoad.length ? familiesToLoad : ['Roboto Slab']
-            },
-            custom: {
-                families: ['Material Icons'],
-                urls: ['./css/fonts.css']
-            },
-            active: function () {
-                defer.resolve()
-            },
-            inactive: function () {
-                //added to make possible ofline template loading
-                defer.resolve()
+            if (!familiesToLoad || !familiesToLoad.length) {
+                familiesToLoad = ['Roboto Slab'];
             }
-        })
+
+            familiesToLoad = familiesToLoad.map(function(font) {
+                return { "fontFamily": font, "variants": ["400"] };
+            });
+
+            _.each(_.filter(manifest.fonts, function(font) { return !font.local; }), function(font) {
+                var fontToLoad = _.find(familiesToLoad, function(fontToLoad) { return font.fontFamily === fontToLoad.fontFamily });
+                if (fontToLoad) {
+                    fontToLoad.variants = _.union(fontToLoad.variants, font.variants);
+                } else {
+                    familiesToLoad.push({ "fontFamily": font.fontFamily, "variants": font.variants })
+                }
+            });
+
+            fontLoader.load({
+                google: {
+                    families: familiesToLoad.map(mapFontName)
+                },
+                custom: {
+                    families: _.filter(manifest.fonts, function(font) { return font.local; }).map(mapFontName),
+                    urls: ['./css/fonts.css']
+                },
+                active: function() {
+                    defer.resolve()
+                },
+                inactive: function() {
+                    //added to make possible ofline template loading
+                    defer.resolve()
+                }
+            });
+        });
         return defer.promise;
+    }
+
+    function mapFontName(fontToLoad) {
+        return fontToLoad.fontFamily + ':' + fontToLoad.variants.join(',');
     }
 });
