23b055c20ecdf1c4fc36b50ac0ff292ea6ba6850
Bohdan Herasymov <b.herasymov@easygenerator.com>
2017-05-08 12:36:59 +0300

    Add logic to work with plugins

--- app/bootstrapping/knockoutBindings/mediaPlayerBinding.js
+++ app/bootstrapping/knockoutBindings/mediaPlayerBinding.js
@@ -1,4 +1,4 @@
-﻿define(['modules/lessProcessor'], function (lessProcessor) {
+﻿define(function () {
     ko.bindingHandlers.mediaPlayer = {
         init: function (element, valueAccessor) {
 
@@ -37,6 +37,6 @@
     };
 
     function getVariablesList() {
-        return lessProcessor.vars ? JSON.stringify({ '@main-color': lessProcessor.vars['@main-color'], '@content-body-color': lessProcessor.vars['@content-body-color'], '@text-color': lessProcessor.vars['@text-color'] }) : undefined;
+        return window.LessProcessor && window.LessProcessor.vars ? JSON.stringify({ '@main-color': window.LessProcessor.vars['@main-color'], '@content-body-color': window.LessProcessor.vars['@content-body-color'], '@text-color': window.LessProcessor.vars['@text-color'] }) : undefined;
     }
 })
