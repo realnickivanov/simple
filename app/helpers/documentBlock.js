bffbab6f19059c89d15a0f3e96a7a573acd5d1e7
yvitiuk <y.vitiuk@easygenerator.com>
2017-07-28 21:42:26 +0300

    fix styles

--- app/helpers/documentBlock.js
+++ app/helpers/documentBlock.js
@@ -1,29 +1,29 @@
 define(['localizationManager', 'constants'], function (localizationManager, constants) {
     return {
         getDocumentBlockContent: function (html) {
-            let $output = $('<output>').html(html),
+            var $output = $('<output>').html(html),
                 $container = $output.find(constants.documents.containerSelector);
 
-            let downloadText = localizationManager.getLocalizedText(constants.documents.downloadLocalizationKey);
-            let documentData = {
+            var downloadText = localizationManager.getLocalizedText(constants.documents.downloadLocalizationKey);
+            var documentData = {
                 type: $container.attr(constants.documents.typeAttrName),
                 size: $container.attr(constants.documents.sizeAttrName)
             };
 
-            let documentSizeString = getSize(documentData.size);
-            let downloadBtnText = `${downloadText} (${documentSizeString})`;
+            var documentSizeString = getSize(documentData.size);
+            var downloadBtnText = downloadText + ' (' + documentSizeString + ')';
 
             $output.find(constants.documents.downloadBtnSelector)
                 .text(downloadBtnText);
 
-            let $typeIcon = $('<div class="icon-container">' +
-                `<span class="document-type-text">${documentData.type}</span>` +
+            var $typeIcon = $('<div class="icon-container">' +
+                '<span class="document-type-text">' + documentData.type + '</span>' +
                 '</div>')
                 .addClass(getIconClass(documentData.type));
-            let $typeIconWrapper = $('<div class="document-icon"></div>')
+            var $typeIconWrapper = $('<div class="document-icon"></div>')
                 .append($typeIcon);
 
-            let $documentInfo = $(constants.documents.documentInfoSelector, $container)
+            var $documentInfo = $(constants.documents.documentInfoSelector, $container)
                 .prepend($typeIconWrapper, $('<div class="separator"></div>'));
 
             return $output.children()[0];
@@ -31,13 +31,16 @@ define(['localizationManager', 'constants'], function (localizationManager, cons
     };
 
     function getIconClass(documentType) {
-        return {
-            [constants.documents.types.zip]: 'icon-zip'
-        }[documentType] || 'icon-file';
+        switch (documentType) {
+            case constants.documents.types.zip:
+                return 'icon-zip';
+            default:
+                return 'icon-file';
+        }
     }
 
     function getSize(sizeKb) {
-        let size = '';
+        var size = '';
         if (!sizeKb) {
             return '0 Kb';
         }
