b50b29827ea2f7a1ca1d9b9826807f29bf284867
ISM-ECOMPANY\o.bulakh <o.bulakh@easygenerator.com>
2017-08-30 15:58:47 +0300

    Fix Incorrect styles for svg

--- app/dialogs/nps/figures/evaluation.js
+++ app/dialogs/nps/figures/evaluation.js
@@ -2,6 +2,7 @@ define(['templateSettings'], function(templateSettings) {
     return {
         textColor: templateSettings.getColorValue('@text-color'),
         correctColor: templateSettings.getColorValue('@correct-color'),
-        contentBodyColor: templateSettings.getColorValue('@content-body-color')
+        contentBodyColor: templateSettings.getColorValue('@content-body-color'),
+        buttonTextColor: templateSettings.getColorValue('@button-text-color')
     };
 });
