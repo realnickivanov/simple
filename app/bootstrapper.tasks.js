6ce5bbb209b7b16769204563feea68cfe71a3f7e
Bohdan_Herasymov <mestl_zz@ukr.net>
2018-01-24 11:30:58 +0200

    Fix black border video iframe (#436)

--- app/bootstrapper.tasks.js
+++ app/bootstrapper.tasks.js
@@ -7,7 +7,8 @@
                 require('bootstrapping/knockoutBindingTask'),
                 require('bootstrapping/binderTask'),
                 require('bootstrapping/routingTask'),
-                require('bootstrapping/reviewTask')
+                require('bootstrapping/reviewTask'),
+                require('bootstrapping/cssCustomBrowserFixTask')
             ];
         }
     };
---
+++ app/bootstrapping/cssCustomBrowserFixTask.js
@@ -0,0 +1,45 @@
+﻿define(function () {
+    "use strict";
+
+    var task = {
+        execute: execute
+    };
+
+    var mainFolder = 'css/';
+
+    var browsers = {
+        ie: {
+            pattern: /MSIE|Trident\//i,
+            css: 'ie.css'
+        },
+        edge: {
+            pattern: /Edge/i,
+            css: 'edge.css'
+        },
+    };
+
+    return task;
+
+    function execute() {
+        for(var key in browsers) {
+            var browser = browsers[key];
+
+            if(browser.pattern.test(navigator.userAgent)) {
+                addStyle(browser.css);
+            }
+        }
+    }
+
+    function addStyle(name) {
+        var head  = document.getElementsByTagName('head')[0];
+        var link  = document.createElement('link');
+
+        link.rel  = 'stylesheet';
+        link.type = 'text/css';
+        link.href = mainFolder + name;
+        link.media = 'all';
+
+        head.appendChild(link);
+    }
+
+});
